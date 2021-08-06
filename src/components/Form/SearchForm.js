d80f1a0f3b09b38e98f550cca4e8f259e2153e11
ph-long <phlong323@gmail.com>
2021-08-05 17:20:54 -0700

    google autocomplete added + needs npm install

--- src/components/Form/SearchForm.js
+++ src/components/Form/SearchForm.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 import './Form.css'
+
 
 const SearchForm = (props) => {
     const [currentStart, setCurrentStart] = useState("")
@@ -9,19 +11,29 @@ const SearchForm = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        props.handleSearch(currentStart,currentEnd,searchTerm)
+        props.handleSearch(currentStart.label,currentEnd.label,searchTerm)
     }
+
 
     return (
         <div className='root'>
             <Form className='form' onSubmit={handleSubmit}>
                 <FormGroup className='group'>
                     <Label for='startingLocation'> Starting Address </Label>
-                    <Input type='address' onChange={(e) => setCurrentStart(e.target.value)}/>
+                    {/* <Input type='address' value={currentStart} onChange={(e) => {setCurrentStart(e.target.value)}}/> */}
+                    <GooglePlacesAutocomplete 
+                        apiKey={process.env.REACT_APP_GOOGLE_MAPS_API}
+                        selectProps={{currentStart, onChange: setCurrentStart,}}
+                        withSessionToken='true'    
+                    />
                 </FormGroup>
                 <FormGroup className='group'>
                     <Label for='destination'> Destination </Label>
-                    <Input type='address' onChange={(e) => setCurrentEnd(e.target.value)}/>
+                    <GooglePlacesAutocomplete 
+                        apiKey={process.env.REACT_APP_GOOGLE_MAPS_API}
+                        selectProps={{currentEnd, onChange: setCurrentEnd,}}
+                        withSessionToken='true'      
+                    />
                 </FormGroup>
                 <FormGroup className='group'>
                     <Label for='pointOfInterest'> Point of Interest </Label>
