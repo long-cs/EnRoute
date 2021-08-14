6224da3f41fa5536859ce20e6c5216655ded144e
Andy <along0095@gmail.com>
2021-08-13 23:40:47 -0700

    Business list height resizes to window size. Removed some unused code

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -4,25 +4,54 @@ import Map from './Map/Map'
 import Businesses from './Businesses/Businesses'
 import { Grid } from '@material-ui/core';
 
+// sets a limit that the function that's passed in is limited to execute every x ms
+// used when resizing Height for Businesses. Resizing for every single pixel lags the browser a bit
+function debounce(fn, ms) {
+    let timer
+    return _ => {
+      clearTimeout(timer)
+      timer = setTimeout(_ => {
+        timer = null
+        fn.apply(this, arguments)
+      }, ms)
+    };
+  }
+  
 // Component mainly to be a container for the maps and businesses
 const Results = (props) => {
+
     const [polyString, setPolyString] = useState([])
     const [businesses, setBusinesses] = useState([])
     const [height, setHeight] = useState(window.innerHeight)
     const [currID, setCurrID] = useState("here")
     const heightStr = height.toString() + "px"
 
-    useEffect(() => {
+    useEffect(() => {  
         if (props.polyline) {
             setPolyString(props.polyline["_polyline_list"])
             setBusinesses(props.polyline["_buisnesses"])
+        }
+
+        // resizes the height of Businesses every 100ms
+        const debouncedHandleResize = debounce(function handleResize() {
+            setHeight(window.innerHeight)
+            // console.log('resized to: ', window.innerWidth, 'x', window.innerHeight)
+          }, 100) //100 ms
+
+        window.addEventListener('resize', debouncedHandleResize) // listen to window resize
+        
+        return function cleanUp() {
+             // remove window listener
+             // If we did not remove listener, then everytime React redraws, it will add another listener
+             // causing it to have hundreds/thousands of listeners if they are never removed
+            window.removeEventListener('resize', debouncedHandleResize)
         }
     }, [props.polyline])
 
     const changeId = (businessId) => {
         setCurrID(businessId)
     }
-
+    
     return (
         <div>
             <Grid container> {/* left half is the map, right half is the list of businesses */}
